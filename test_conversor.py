2d502a0f6008bb47d9b172e6292c68aad1176014
Reticencia <ezequielangel141@gmail.com>
2026-09-16 16:40:06 -0600

    Agregue otro id por que no me gusto el que tenia

--- test_conversor.py
+++ test_conversor.py
@@ -1,7 +1,8 @@
 import pytest 
 from conversor import celsius_fahrenheit, kilometros_millas, pesos_dolares
 
-#ID:1Prueba parametros y con marker unitario
+#ID:PR1
+#Prueba parametros y con marker unitario
 @pytest.mark.unit
 @pytest.mark.parametrize("entrada_celsius, resultado_fahrenheit", [
     (0, 32.00),
@@ -12,14 +13,16 @@ from conversor import celsius_fahrenheit, kilometros_millas, pesos_dolares
 def test_celsius_fahrenheit(entrada_celsius, resultado_fahrenheit):
     assert celsius_fahrenheit(entrada_celsius) == resultado_fahrenheit
 
-#ID:2Prueba de distancia
+#ID:PR2
+# Prueba de distancia
 @pytest.mark.unit
 def test_kilometros_millas():
     assert kilometros_millas(1) == 0.62
     assert kilometros_millas(5) == 3.11
     assert kilometros_millas(10) == 6.21
 
-#ID:3Prueba de pesos a dolares
+#ID:PR3
+# Prueba de pesos a dolares
 @pytest.mark.unit
 def test_pesos_dolares():
     assert pesos_dolares(18.50) == 1.00
